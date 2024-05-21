35f42f8494d752a9a65d19d6dab2240a960b35b8
Florent Menus <florent.mns@gmail.com>
2024-05-21 13:26:14 +0200

    staticParams config 1

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,3 +1,7 @@
+export async function generateStaticParams() {
+  return [{ slug: "test" }];
+}
+
 export default function Test({ params }: { params: { slug: string } }) {
   return <p>{params.slug}</p>;
 }
