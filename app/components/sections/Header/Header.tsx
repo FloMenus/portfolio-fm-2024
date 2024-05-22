02d6e3e5869e42f5cc033e5341e5a184b9b9f93d
Florent Menus <florent.mns@gmail.com>
2024-05-22 19:42:01 +0200

    done header, about & skills sections, started projects section

--- app/components/sections/Header/Header.tsx
+++ app/components/sections/Header/Header.tsx
@@ -1,14 +1,45 @@
 import style from "./style.module.css";
+import img from "next/image";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Header() {
   return (
     <header className={style.header}>
-      <h1 className={style.title}>FLORENT MENUS</h1>
-      <h2 className={style.subtitle}>DÉVELOPPEUR JUNIOR</h2>
-      <div className={style.buttonsGroup}>
-        <button className={style.button_1}>Me contacter</button>
-        <button className={style.button_2}>Télécharger mon CV</button>
+      <p className={style.hello_text}>
+        BONJOUR, <span>JE SUIS</span>
+      </p>
+      <div className={style.text}>
+        <h1 className={style.title}>FLORENT MENUS</h1>
+        <h2 className={style.subtitle}>DÉVELOPPEUR JUNIOR</h2>
+        <Image
+          src="/assets/shape2.svg"
+          className={style.shape2}
+          alt="shape2"
+          width={434}
+          height={489}
+        />
+        <Image
+          src="/assets/shape3.svg"
+          className={style.shape3}
+          alt="shape3"
+          width={1008}
+          height={629}
+        />
       </div>
+      <div className={style.buttons_group}>
+        <button className={style.button_1}>Me contacter</button>
+        <a className={style.button_2} href="/test.png" download>
+          Télécharger mon CV
+        </a>
+      </div>
+      <Image
+        src="/assets/shape1.svg"
+        className={style.shape1}
+        alt="shape1"
+        width={2550}
+        height={179}
+      />
     </header>
   );
 }
---
+++ app/components/sections/Skills/SkillsContainer/SkillsContainer.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import style from "./style.module.css";
+import Image from "next/image";
+import { useState } from "react";
+
+const skills: { name: string; image: string }[] = [
+  { name: "HTML5", image: "html" },
+  { name: "CSS3", image: "css" },
+  { name: "JavaScript", image: "javascript" },
+  { name: "TypeScript", image: "typescript" },
+  { name: "React", image: "react" },
+  { name: "Next.js", image: "nextjs" },
+  { name: "SaSS", image: "sass" },
+  { name: "MaterialUI", image: "mui" },
+  { name: "Tailwind CSS", image: "tailwind" },
+  { name: "Redux", image: "redux" },
+  { name: "Node.js", image: "nodejs" },
+  { name: "Express.js", image: "express" },
+  { name: "MySQL", image: "mysql" },
+  { name: "MongoDB", image: "mongodb" },
+  { name: "Nest.js", image: "nest" },
+  { name: "Jest", image: "jest" },
+  { name: "Git", image: "git" },
+  { name: "Figma", image: "figma" },
+  { name: "Photoshop", image: "photoshop" },
+  { name: "Illustrator", image: "illustrator" },
+];
+
+export default function SkillsContainer() {
+  const [selectedSkill, setSelectedSkill]: [
+    string,
+    (selectedSkill: string) => void
+  ] = useState("");
+
+  return (
+    <div className={style.skills_container}>
+      {skills.map((skill) => (
+        <div className={style.skill} key={skill.name}>
+          <Image
+            key={skill.name}
+            src={`/assets/skills/${skill.image}.png`}
+            alt={skill.name}
+            className={style.skill_image}
+            width={86}
+            height={85}
+            onMouseEnter={() => {
+              setSelectedSkill(skill.name);
+            }}
+            onMouseLeave={() => {
+              selectedSkill === skill.name && setSelectedSkill("");
+            }}
+          />
+
+          <div
+            className={
+              selectedSkill === skill.name
+                ? `${style.message_box} ${style.displayed}`
+                : style.message_box
+            }
+          >
+            {skill.name}
+          </div>
+        </div>
+      ))}
+      {/* <Image
+        src="/assets/shape5.svg"
+        alt="shape5"
+        className={style.shape5}
+        width={677}
+        height={770}
+      /> */}
+    </div>
+  );
+}
