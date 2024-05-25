c0c3dfe852326fcda4ade5d34490cb990d4dd4fa
Florent Menus <florent.mns@gmail.com>
2024-05-25 14:28:57 +0200

    header buttons functionnalities

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,7 +48,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={hind.className}>
+    <html lang="fr" className={hind.className}>
       <body>{children}</body>
     </html>
   );
