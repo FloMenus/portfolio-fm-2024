13f2a57be22d2c84a71b20cc6e0c73c64ee3aeb3
Florent Menus <florent.mns@gmail.com>
2024-05-26 16:56:03 +0200

    added projects

--- app/components/sections/Projects/Projects.tsx
+++ app/components/sections/Projects/Projects.tsx
@@ -4,14 +4,29 @@ import Link from "next/link";
 const projects: { title: string; link: string; description: string }[] = [
   {
     title: "Site vitrine Yvea",
-    link: "/yvea",
+    link: "https://fm-yvea-landing.onrender.com/",
     description:
       "Site vitrine pour une plateforme de gestion automatisée de certificats d'export de marchandises",
   },
   {
     title: "Moovice",
-    link: "/moovice",
+    link: "https://moovice-flomenus.netlify.app/",
     description: "Site de référencement de films, à la manière d'un AlloCiné",
+  },
+  {
+    title: "Portfolio",
+    link: "https://florent-menus.com/",
+    description: "Portfolio de Florent Menus",
+  },
+  {
+    title: "Todo list",
+    link: "https://cosmic-paletas-7dd7b8.netlify.app/",
+    description: "Application de gestion de tâches",
+  },
+  {
+    title: "Généraeur Pokémon",
+    link: "https://random-pokemon-flomenus.netlify.app/",
+    description: "Générateur de Pokémon aléatoire",
   },
 ];
 
@@ -30,6 +45,7 @@ export default function Projects() {
                 // href={"projects" + project.link}
                 href={project.link}
                 className={style.project_link}
+                target="_blank "
               >
                 Consulter
               </Link>
